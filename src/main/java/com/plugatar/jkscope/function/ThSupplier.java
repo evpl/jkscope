56222c72ff1d6d790bc8d5239e3813d35c3dc09f
Evgenii Plugatar <evpl.dev@gmail.com>
2025-01-25 14:46:49 +0300

    updated copyright

--- src/main/java/com/plugatar/jkscope/function/ThSupplier.java
+++ src/main/java/com/plugatar/jkscope/function/ThSupplier.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 Evgenii Plugatar
+ * Copyright 2024-2025 Evgenii Plugatar
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
